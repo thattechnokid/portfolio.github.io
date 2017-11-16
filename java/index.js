35378d89eda0658fdb1c6f7a390b138ba2f6e99e
Cristian Mendoza <thattechnokid@gmail.com>
2017-11-15 16:17:50 -0800

    Fixed Typed JS, <h*> tags, and cursor animation

--- java/index.js
+++ java/index.js
@@ -2,5 +2,18 @@ $(document).ready(function(){
   $("#clickToScroll").animate({
     bottom:'75px',
     opacity:'1.0'
-  },2000);
+  },500);
+
+
+    var options = {
+      strings: ["I Build Websites", "I Build Web Applications"],
+      startDelay: 1000,
+      typeSpeed: 70,
+      backDelay:1000,
+      smartBackspace: true,
+      backSpeed: 60
+    }
+    var typed = new Typed(".typed-txt", options);
+
+
 });
